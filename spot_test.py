d46dcd34a39a6fe834a2b96fb04bc3b0455f5587
daehyun95 <wjdeogus03@gmail.com>
2022-08-21 00:59:55 -0400

    rm

--- spot_test.py
+++
@@ -1,9 +0,0 @@
-from spot import Spot
-
-
-def test_constructor():
-    spot = Spot(100, 100, 255, 20)
-    assert spot.x == 100
-    assert spot.y == 100
-    assert spot.stone_color == 255
-    assert spot.stone_size == 20
